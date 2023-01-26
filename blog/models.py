8b571a9c5989d60c96f11daaffe1203bc4770a70
Vasilina Alexeeva <fomina.vasilina.93@gmail.com>
2023-01-26 20:44:52 +0600

    some 111

---
+++ blog/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+# Create your models here.
+
+class Post(models.Model):
+    post_title = models.CharField(max_length=300)
+    post_data = models.DateTimeField()
+    post_text = models.TextField()
+    post_image = models.ImageField(upload_to='post_images/')
+
+    def get_summary(self):
+        return self.post_text[:70]
+
+    def __str__(self):
+        return self.post_title
